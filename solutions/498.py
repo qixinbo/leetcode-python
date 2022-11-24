81126e115939245bf443144202a03257995ae66f
qixinbo <qixinbo@gmail.com>
2022-11-24 16:00:23 +0800

    add 498

---
+++ solutions/498.py
@@ -0,0 +1,34 @@
+class Solution:
+    def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
+        rows = len(mat)
+        cols = len(mat[0])
+        count = rows * cols
+        x, y = 0, 0
+        ans = []
+
+        for i in range(count):
+            ans.append(mat[x][y])
+
+            if (x + y) % 2 == 0:
+                # 最后一列
+                if y == cols - 1:
+                    x += 1
+                # 第一行
+                elif x == 0:
+                    y += 1
+                # 右上方向
+                else:
+                    x -= 1
+                    y += 1
+            else:
+                # 最后一行
+                if x == rows - 1:
+                    y += 1
+                # 第一列
+                elif y == 0:
+                    x += 1
+                # 左下方向
+                else:
+                    x += 1
+                    y -= 1
+        return ans
